631776c36dda063e2f9ef6e4ce28815ad706d392
ImadEM21 <imad.elmahrad98@gmail.com>
2021-01-01 18:56:25 +0100

    add some styles for the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,16 +8,16 @@ const NavBar = (props) => {
         <>
         <Navbar className="no-bg border-bottom border-white" expand="lg" >
             <Navbar.Brand href="/">
-                <FontAwesomeIcon icon={faUserAstronaut} color="white" size="4x"/>
+                <FontAwesomeIcon icon={faUserAstronaut} color="white" size="4x" className="icon-transform"/>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-                <Nav.Link href="#presentation" className="text-white">NASA</Nav.Link>
-                <Nav.Link href="#earth" className="text-white">La Terre</Nav.Link>
-                <Nav.Link href="#mars" className="text-white">Mars</Nav.Link>
-                <Nav.Link href="/space" className="text-white">Système solaire</Nav.Link>
-                <Nav.Link href="/stargate" className="text-white">Stargate</Nav.Link>
+                <Nav.Link href="#presentation" className="text-white nav-hover">NASA</Nav.Link>
+                <Nav.Link href="#earth" className="text-white nav-hover">La Terre</Nav.Link>
+                <Nav.Link href="#mars" className="text-white nav-hover">Mars</Nav.Link>
+                <Nav.Link href="/space" className="text-white nav-hover">Système solaire</Nav.Link>
+                <Nav.Link href="/stargate" className="text-white nav-hover">Stargate</Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Navbar>
